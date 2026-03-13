b78b3febeb3edfdf062338ee667d6899525b3ead
alonsog88 <g88alonso@gmail.com>
2026-03-12 20:18:42 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,9 @@ import compress from "astro-compress";
 import icon from "astro-icon";
 import { defineConfig } from 'astro/config';
 
-import vercel from "@astrojs/vercel";
-
-// https://astro.build/config
 export default defineConfig({
-  site: "https://alonsog88.github.io", // 👈 add this
+  site: "https://alonsog88.github.io",
+  base: "/your-repo-name",
   integrations: [tailwind(), icon(), compress()],
-  output: "server",
-  adapter: vercel()
+  output: "static",  // 👈 changed from "server"
 });
